e81ae5280174db8e6a04fb3d174ae5ccb7c39fe5
Jklim12 <julianova1987@gmail.com>
2024-10-21 19:44:31 +0300

    Add files via upload

---
+++ modyl_1_task_4.py
@@ -0,0 +1,6 @@
+#my_string= input()#
+print('my_string'.upper())
+print('my_string'.capitalize())
+print('my_string'.replace('_',''))
+print('my_string'.replace('string',''))
+print('my_string'.replace('my_',''))
